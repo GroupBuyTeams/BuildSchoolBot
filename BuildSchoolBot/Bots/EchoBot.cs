d1dd54ae182465fd957acee1866f19652c0587f8
Eileen <a0908802569@gmail.com>
2020-07-28 14:58:01 +0800

    delete Cards

--- BuildSchoolBot/Bots/EchoBot.cs
+++ BuildSchoolBot/Bots/EchoBot.cs
@@ -19,6 +19,7 @@ using System;
 using System.IO;
 using Newtonsoft.Json;
 using BuildSchoolBot.Service;
+using Microsoft.Bot.Builder.Dialogs;
 
 namespace BuildSchoolBot.Bots
 {
@@ -28,59 +29,8 @@ namespace BuildSchoolBot.Bots
         //ting 開團
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
-            if (string.Equals(turnContext.Activity.Text, "Let's buy something", System.StringComparison.InvariantCultureIgnoreCase))
-            {
-                var add = MessageFactory.Text("No problem"); //回傳文字 
+            
 
-                string card = "{\r\n    \"type\": \"AdaptiveCard\",\r\n    \"$schema\": \"\",\r\n    \"version\": \"1.2\",\r\n    \"body\": [\r\n        {\r\n            \"type\": \"TextBlock\",\r\n            \"text\": \"Input your Address:\"\r\n        },\r\n        {\r\n            \"type\": \"Input.Text\",\r\n            \"placeholder\": \"Address\",\r\n            \"id\": \"address\"\r\n        },\r\n        {\r\n            \"type\": \"ActionSet\",\r\n            \"actions\": [\r\n                {\r\n                    \"type\": \"Action.Submit\",\r\n                    \"title\": \"Confirm\"\r\n                }\r\n            ]\r\n        }\r\n    ]\r\n}";
-
-                try
-                {
-                    var parsedResult = AdaptiveCard.FromJson(card);
-                    var attachment = new Attachment
-                    {
-                        ContentType = AdaptiveCard.ContentType,
-                        Content = parsedResult.Card,
-                        
-                    };
-                    add.Attachments.Add(attachment);
-                    
-                }
-                catch (AdaptiveSerializationException e)
-                {
-                    throw;
-                }
-
-                await turnContext.SendActivityAsync(add, cancellationToken);//機器人傳送文字訊息
-               
-            }
-            //抓地址
-            string ob;
-            if(turnContext.Activity.Value != null) { //如果輸入框不是null
-                ob = turnContext.Activity.Value.ToString(); //ob 等於輸入框轉成的字串
-                //var input = Console.ReadLine();//輸入地址並印出來
-                string input = JsonConvert.DeserializeObject<Address>(ob).Add;
-                var key = "AIzaSyAlKWP4uWjQIR3WDAWLAu6rUhBfc3_ppag";//金鑰
-                string requestUri = string.Format("https://maps.googleapis.com/maps/api/geocode/xml?key={1}&address={0}&sensor=false", Uri.EscapeDataString(input), key);
-                //string.Format將後面的物件轉成字串插入格式字串中的指定位置
-                WebRequest request = WebRequest.Create(requestUri);//創建一個新的請求
-                WebResponse response = request.GetResponse();//去拿回剛剛建的request請求,並GET API回傳的值 
-                Stream responsestream = response.GetResponseStream(); //獲得response 裡的stream資料流
-                XDocument xdoc = XDocument.Load(responsestream);//從XDocument指定的資料流,放在xdoc裡
-
-                XElement result = xdoc.Element("GeocodeResponse").Element("result");
-                XElement locationElement = result.Element("geometry").Element("location");
-
-                string lat = locationElement.Element("lat").Value;
-                string lng = locationElement.Element("lng").Value;
-
-                var latlng = new WebCrawler();//使用爬蟲的方法
-
-                await latlng.GetStores(lat, lng);//抓到經緯度
-
-                var replyText = $"Echo: {turnContext.Activity.Text}. OK";//接收文字訊息
-                await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
-            }
         }
 
         //添加成員會跑這個方法

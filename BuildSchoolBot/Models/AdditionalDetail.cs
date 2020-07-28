227cee44156f064a9e5528b369cbbe615735f496
a0975283413@gmail.com <a0975283413@gmail.com>
2020-07-28 14:59:45 +0800

    add models

---
+++ BuildSchoolBot/Models/AdditionalDetail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BuildSchoolBot.Models
+{
+    public partial class AdditionalDetail
+    {
+        public Guid AddId { get; set; }
+        public string AddItem { get; set; }
+
+        public virtual Additional Add { get; set; }
+    }
+}
---
+++ BuildSchoolBot/Models/Library.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BuildSchoolBot.Models
+{
+    public partial class Library
+    {
+        public Guid LibraryId { get; set; }
+        public string MenuUri { get; set; }
+        public Guid MemberId { get; set; }
+    }
+}

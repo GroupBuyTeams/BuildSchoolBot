f3f5b055b3c2d82a4114d35f6b0fa8be27e284ca
Afan <kpopganggg@gmail.com>
2020-07-30 18:12:16 +0800

    0730orderfood

---
+++ BuildSchoolBot/StoreModels/GetStore.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuildSchoolBot.StoreModels
+{
+    public class GetStore
+    {
+          public class Storenameitems
+        {
+            public string StoreName { get; set; }
+            public string menuproperties { get; set; }
+        }
+
+        //public class Storenamegroup
+        //{
+        //    public List<Storenameitems> properties { get; set; }
+        //}
+    }
+}
---
+++ BuildSchoolBot/StoreModels/fooditem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuildSchoolBot.StoreModels
+{
+    public class fooditem
+    {
+        public class fooditems
+        {
+            public string key { get; set; }
+            public string value { get; set; }
+        }
+
+        public class foodgroup
+        {
+            public List<fooditems> properties { get; set; }
+        }
+    }
+}

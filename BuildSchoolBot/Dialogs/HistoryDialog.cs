5d0f9d0c1600b174310996ac96f3baea14399262
a0975283413@gmail.com <a0975283413@gmail.com>
2020-08-03 19:31:00 +0800

    history

--- BuildSchoolBot/Dialogs/HistoryDialog.cs
+++ BuildSchoolBot/Dialogs/HistoryDialog.cs
@@ -2,9 +2,11 @@
 using BuildSchoolBot.Models.HistoryModels;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Choices;
 using Microsoft.Bot.Schema;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -17,24 +19,66 @@ namespace BuildSchoolBot.Dialogs
 {
     public class HistoryDialog : ComponentDialog
     {
+        protected readonly Dialog Dialog;
 
         public HistoryDialog() : base()
         {
+
             var waterfallSteps = new WaterfallStep[]
             {
-                DateSelectStepAsync
+                DateSelectStepAsync,
+                HandleResponseAsync
             };
             AddDialog(new WaterfallDialog(nameof(WaterfallDialog), waterfallSteps));
+            AddDialog(new TextPrompt(nameof(TextPrompt)));
+            AddDialog(new NumberPrompt<int>(nameof(NumberPrompt<int>)));
+            AddDialog(new ChoicePrompt(nameof(ChoicePrompt)));
+            AddDialog(new ConfirmPrompt(nameof(ConfirmPrompt)));
+            AddDialog(new AttachmentPrompt(nameof(AttachmentPrompt)));
             InitialDialogId = nameof(WaterfallDialog);
         }
 
         private async Task<DialogTurnResult> DateSelectStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            //return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = MessageFactory.Attachment(CreateAdaptiveCardUsingJson()) }, cancellationToken);
+            var message = MessageFactory.Text("");
+            message.Attachments = new List<Attachment>() { CreateAdaptiveCardUsingJson() };
+            await stepContext.Context.SendActivityAsync(message, cancellationToken);
 
-            //await stepContext.NextAsync();.Context.SendActivityAsync(MessageFactory.Attachment(CreateAdaptiveCardUsingJson()), cancellationToken);
+            var opts = new PromptOptions
+            {
+                Prompt = new Activity
+                {
+                    Type = ActivityTypes.Message,
+                    Text = "waiting for user input..."
+                }
+            };
+
+            return await stepContext.PromptAsync(nameof(TextPrompt), opts);
         }
 
+        private async Task<DialogTurnResult> HandleResponseAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            // Do something with step.result
+            // Adaptive Card submissions are objects, so you likely need to JObject.Parse(step.result)
+            await stepContext.Context.SendActivityAsync($"INPUT: {stepContext.Result}");
+            return await stepContext.NextAsync();
+        }
+
+        protected async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            // Capture input from adaptive card
+            if (string.IsNullOrEmpty(turnContext.Activity.Text) && turnContext.Activity.Value != null)
+            {
+                // Conditionally convert based off of input ID of Adaptive Card
+                if ((turnContext.Activity.Value as JObject)["<adaptiveCardInputId>"] != null)
+                {
+                    turnContext.Activity.Text = (turnContext.Activity.Value as JObject)["<adaptiveCardInputId>"].ToString();
+                }
+            }
+        }
+
+
+        //產生卡片
         private Attachment CreateAdaptiveCardUsingJson()
         {
             var paths = new[] { ".", "Resources", "DateSelectCard.json" };

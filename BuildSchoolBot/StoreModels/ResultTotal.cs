9dcc873bb761f43259875e5a2f3a286da02460f7
Afan <kpopganggg@gmail.com>
2020-08-07 11:26:45 +0800

    0807總結卡片

---
+++ BuildSchoolBot/StoreModels/ResultTotal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuildSchoolBot.StoreModels
+{
+    public class ResultTotal
+    {
+        public class ResultTotalItem
+        {
+            public int Quantity { get; set; }
+            public string UserName { get; set; }
+            public string Dish_Name { get; set; }
+            public decimal Price { get; set; }
+            public string MemberId { get; set; }
+        }
+        public class ResultTotalItemsGroup
+        {
+            public List<ResultTotalItem> TotalItemsGroup { get; set; }
+        }
+        public class AllTotalItemsGroups
+        {
+            public List<ResultTotalItemsGroup> AllTotalItems { get; set; }
+        }
+
+    }
+}

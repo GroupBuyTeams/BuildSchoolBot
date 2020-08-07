d3fb5388fbab2b3296b56e5d815771b3b395037c
Eileen <a0908802569@gmail.com>
2020-08-07 22:17:44 +0800

    新增model

---
+++ BuildSchoolBot/Service/MenuService.cs
@@ -0,0 +1,25 @@
+﻿using BuildSchoolBot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuildSchoolBot.Service
+{
+    public class MenuService
+    {
+        public TeamsBuyContext context;
+        public void CreateMenu(string _menuId)
+        {
+            var menu = new MenuOrder
+            {
+                MenuId = Guid.Parse(_menuId),
+                //Store = ,
+                //TeamsId = ,
+
+            };
+            context.MenuOrder.Add(menu);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ BuildSchoolBot/Service/PayMentService.cs
@@ -0,0 +1,24 @@
+﻿using BuildSchoolBot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuildSchoolBot.Service
+{
+    public class PayMentService
+    {
+        public TeamsBuyContext context;
+
+        public void Update(string _memberId)
+        {
+            var pay = new Payment
+            {
+                MemberId = _memberId,
+                //Url = 
+            };
+            context.Payment.Add(pay);
+            context.SaveChanges();
+        }
+    }
+}

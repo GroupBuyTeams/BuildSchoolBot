87c4dce0a06f5d766cf972df3f281fde700cc50d
Eileen <a0908802569@gmail.com>
2020-08-10 17:24:40 +0800

    重心拉model

--- BuildSchoolBot/Models/MenuOrder.cs
+++ BuildSchoolBot/Models/MenuOrder.cs
@@ -5,10 +5,15 @@ namespace BuildSchoolBot.Models
 {
     public partial class MenuOrder
     {
+        public MenuOrder()
+        {
+            MenuDetail = new HashSet<MenuDetail>();
+        }
+
         public Guid MenuId { get; set; }
         public string Store { get; set; }
         public string TeamsId { get; set; }
 
-        public virtual MenuDetail Menu { get; set; }
+        public virtual ICollection<MenuDetail> MenuDetail { get; set; }
     }
 }

809e7d5fb947bca93080dd480baaacb38eff6e5e
mizugakun <mizugakun@gmail.com>
2020-08-14 17:47:01 +0800

    修正 MainDialog無法觸發ReservationDialog的問題

--- BuildSchoolBot/Dialogs/MainDialog.cs
+++ BuildSchoolBot/Dialogs/MainDialog.cs
@@ -50,8 +50,10 @@ namespace BuildSchoolBot.Dialogs
                     return await stepContext.BeginDialogAsync(nameof(AddressDialogs));
                 case "History":
                     return await stepContext.BeginDialogAsync(nameof(HistoryDialog));
+                case "Reserve":
+                    return await stepContext.BeginDialogAsync(nameof(ReservationDialog));
                 default:
-                    await stepContext.Context.SendActivityAsync(MessageFactory.Text("Your choise is not 'Buy'."));
+                    await stepContext.Context.SendActivityAsync(MessageFactory.Text("Your choice is not 'Buy'."));
                     return await stepContext.NextAsync();
             }
 
